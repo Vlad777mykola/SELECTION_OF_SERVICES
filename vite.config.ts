90731bd6c55ab18985732488b8d072b6546c3bf4
Vlad777mykola <vlad777mykola@gmail.com>
2025-04-16 19:54:25 +0300

    feat(rename): project

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'selection_of_services',
+  base: 'SELECTION_OF_SERVICES',
 });
